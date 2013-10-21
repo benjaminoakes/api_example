381874cedc53800e729a511bf5e6e9a14c6d3524
Benjamin Oakes <hello@benjaminoakes.com>
2013-10-21 16:58:28 -0500

    Add some docco-style comments

--- app.rb
+++ app.rb
@@ -1,15 +1,20 @@
 require 'httparty'
 require 'sinatra'
 
+# This is just a simple HTTP library which gives methods for HTTP verbs like `GET` and `POST`.
 class GitHub
   include HTTParty
   base_uri 'https://api.github.com'
 end
 
+# This is the root path, which has links to the other examples in this file.
+#
+# If you're running this on your machine, [start here](http://localhost:4567)!
 get '/' do
   erb :root
 end
 
+# This gets user information and renders it into HTML.
 get '/github/users/:username' do
   user = GitHub.get("/users/#{params[:username]}")
   erb :user, :locals => user
